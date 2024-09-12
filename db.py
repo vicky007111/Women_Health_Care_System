94fa785408864c6b5c8b10d96fda6b77632689fc
vicky007111 <151443182+vicky007111@users.noreply.github.com>
2024-09-12 23:10:33 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 import os
 
 # Load environment variables
-DATABASE_URL = os.getenv('MONGODB_URI', 'mongodb+srv://vigneshselvaa9940:wPboi3avrKoZtjaE@cluster0.qlzkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+DATABASE_URL = os.getenv('MONGODB_URI', 'mongodb+srv://vigneshselvaa9940:iambatman@cluster0.qlzkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 # Create a MongoDB client
 client = MongoClient(DATABASE_URL)
